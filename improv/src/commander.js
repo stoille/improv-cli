341029c16a1de3f950c2ad8ae6ba9621080e1fc2
mark <mark@wave255.com>
2018-05-21 23:32:19 -0700

    Refactor: move commander.parseScript into commands

--- improv/src/commander.js
+++ improv/src/commander.js
@@ -14,20 +14,7 @@ program
   .action(scriptPath => {
     let scriptText = fs.readFile(scriptPath, "utf8", (err, text) => {
       let parsedScript = parseScript(text)
-      var cache = []
-      console.log(JSON.stringify(parsedScript, (key, value) => {
-        if (typeof value === 'object' && value !== null) {
-          if (cache.indexOf(value) !== -1) {
-            // Circular reference to parent found, discard key before printing
-            if(key === 'parent')
-              return
-          }
-          // Store value in our collection
-          cache.push(value)
-        }
-        return value;
-      }))
-      cache = null
+      console.log(parsedScript)
       process.exit(0)
     })
   })
