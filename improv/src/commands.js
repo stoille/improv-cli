05b3d22fcf5f0bad26ca49159447027ba7c7972f
shar <anne@wave255.com>
2018-05-21 23:32:19 -0700

    Refactor: move commander.parseScript into commands

--- improv/src/commands.js
+++ improv/src/commands.js
@@ -1,8 +1,25 @@
 const { parseLines } = require("./parser")
 
+var cache = []
+
 const parseScript = (text) => {
   let t = text.split('\n')
-  return parseLines(t)
+  let parsedScript = parseLines(t)
+  cache = []
+  return JSON.stringify(parsedScript, filterParentRefences)
 }
-
 module.exports.parseScript = parseScript
+
+function filterParentRefences(key, value) {  
+  if (typeof value === 'object' && value !== null) {
+    if (cache.indexOf(value) !== -1) {
+      // Circular reference to parent found, discard key before printing
+      if (key === 'parent')
+        return
+    }
+    // Store value in our collection
+    cache.push(value)
+  }
+  
+  return value;
+}
