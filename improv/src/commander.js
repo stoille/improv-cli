28cdd36723104191a14b0546af698a51f182365b
mark <mark@wave255.com>
2018-05-24 22:01:59 -0700

    Refactor: commander listens for -j and --json option to print to json instead of default node printer

--- improv/src/commander.js
+++ improv/src/commander.js
@@ -3,15 +3,14 @@ const program = require('commander')
 const { readScriptFileAndParse } = require('./commands')
 
 program
-  .version('0.0.1')
+  .version('0.1.0')
   .description('improv compiler')
-
-program
   .command('parse <scriptPath>')
   .alias('p')
   .description('parses an .improv script file')
-  .action(scriptPath => {
-    readScriptFileAndParse(scriptPath)
+  .option('-j, --json', 'print to JSON')
+  .action((scriptPath, cmd) => {
+    readScriptFileAndParse(scriptPath, cmd.json)
     .then(parsedScript => {
       console.log(parsedScript)
       process.exit(0)
