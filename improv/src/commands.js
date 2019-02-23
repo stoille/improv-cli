7b8f3545e411f9818cb515fe1be93f0e1f4d0098
shar <anne@wave255.com>
2019-02-23 01:19:00 -0800

    *added support to chain conditionals using '-' as AND and multi-line conds as OR expressions TODO: fix top level state transition condition TODO: text data of lines being placed in done

--- improv/src/commands.js
+++ improv/src/commands.js
@@ -14,8 +14,8 @@ const parseScript = (text, printToJSON) => {
 module.exports.parseScript = parseScript
 
 function printScript(parsedScript) {
-  cache = []
-  return JSON.stringify(parsedScript, filterParentRefences)
+  isCyclic(parsedScript)
+  return JSON.stringify(parsedScript)
 }
 
 const readScriptFileAndParse = (scriptPath, printToJSON) => new Promise((resolve, reject) =>
@@ -29,18 +29,42 @@ const readScriptFileAndParse = (scriptPath, printToJSON) => new Promise((resolve
   }))
 module.exports.readScriptFileAndParse = readScriptFileAndParse
 
-var cache = []
+function isCyclic(obj) {
+  var keys = [];
+  var stack = [];
+  var stackSet = new Set();
+  var detected = false;
 
-function filterParentRefences(key, value) {  
-  if (typeof value === 'object' && value !== null) {
-    if (cache.indexOf(value) !== -1) {
-      // Circular reference to parent found, discard key before printing
-      if (key === 'parent')
-        return
+  function detect(obj, key) {
+    if (obj && typeof obj != 'object') {
+      return;
     }
-    // Store value in our collection
-    cache.push(value)
+
+    if (stackSet.has(obj)) { // it's cyclic! Print the object and its locations.
+      var oldindex = stack.indexOf(obj);
+      var l1 = keys.join('.') + '.' + key;
+      var l2 = keys.slice(0, oldindex + 1).join('.');
+      console.log('CIRCULAR: ' + l1 + ' = ' + l2 + ' = ' + obj);
+      console.log(obj);
+      detected = true;
+      return;
+    }
+
+    keys.push(key);
+    stack.push(obj);
+    stackSet.add(obj);
+    for (var k in obj) { //dive on the object's children
+      if (Object.prototype.hasOwnProperty.call(obj, k)) {
+        detect(obj[k], k);
+      }
+    }
+
+    keys.pop();
+    stack.pop();
+    stackSet.delete(obj);
+    return;
   }
-  
-  return value;
+
+  detect(obj, 'obj');
+  return detected;
 }
