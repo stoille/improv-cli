064b59c38934e889917c747572c9b62278d92504
shar <anne@wave255.com>
2019-04-21 23:07:09 -0700

    *Starting to clean up guards -Removed stale guards
    *Added toJS/toJSON options to commander
    *Removed --toXML and --print options from commander
    
    -Refactored names of loading functions to reflect they are 'hints' not blockers

--- improv/src/commander.js
+++ improv/src/commander.js
@@ -3,15 +3,14 @@ const program = require('commander')
 const { readScriptFileAndParse } = require('./commands')
 
 program
-  .version('0.1.0')
+  .version('0.2.0')
   .description('improv compiler')
   .command('parse <scriptPath>')
   .description('parses an improv (.imp) script file')
-  .option('-j, --json', 'export to JSON')
-  .option('-x, --xml', 'export to XML')
-  .option('-p, --print', 'print to console')
+  .option('-j, --json', 'print to JSON')
+  .option('-js, --js', 'print to JS')
   .action((scriptPath, cmd) => {
-    readScriptFileAndParse(scriptPath, cmd.json, cmd.xml, cmd.print, )
+    readScriptFileAndParse(scriptPath, cmd.json, cmd.js)
     .then(parsedScript => {
       console.log(parsedScript)
       process.exit(0)
