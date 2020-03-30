14d1cfcabc2d2ef923c9190430a67491b6a5a1f9
brian <daedalicarts@gmail.com>
2020-03-29 23:41:32 -0700

    *Refactored all timing to use MS instead of min/sec for better shot timing accuracy *Added support for timeSpan milliseconds *All states with timing on them now will have time inherited from their parents rather than be 0

--- improv/src/commands.js
+++ improv/src/commands.js
@@ -2,9 +2,9 @@ const { jsonToXStateMachine, impToStream } = require("./parser")
 const util = require('util')
 const fs = require('fs')
 
-async function parseScript(filePath, text, ops) {
-  let t = text.split('\n')
-  let stream = await impToStream(filePath, t, readScriptFileAndParse)
+async function parseScript(filePath, text, ops, parentState) {
+  let lines = text.split('\n')
+  let stream = await impToStream(filePath, readScriptFileAndParse, lines, parentState)
   if (ops.json) {
     return JSON.stringify(stream)
   } else if (ops.js) {
@@ -14,9 +14,9 @@ async function parseScript(filePath, text, ops) {
 }
 module.exports.parseScript = parseScript
 
-async function readScriptFileAndParse(scriptPath, ops) {
+async function readScriptFileAndParse(scriptPath, ops, parentState) {
   const readFile = util.promisify(fs.readFile)
   let rawText = await readFile(scriptPath, 'utf8')
-  return parseScript(scriptPath, rawText, ops)
+  return parseScript(scriptPath, rawText, ops, parentState)
 }
 module.exports.readScriptFileAndParse = readScriptFileAndParse
